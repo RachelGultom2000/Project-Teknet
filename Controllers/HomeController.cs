9a7abf9ad4deba16c10ccc31c9770f8e9a37afcb
nick2905 <nicolasmanurung16@gmail.com>
2020-05-08 23:03:14 +0700

    Bank Konservatif Frondend Implementation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,71 @@ namespace Travelancar.Controllers
             return View();
         }
 
+        public ActionResult BankLogin()
+        {
+            ViewBag.Message = "Bank Login";
+
+            return View();
+        }
+
+        public ActionResult BankDashboard()
+        {
+            ViewBag.Message = "Dashboard Bank";
+
+            return View();
+        }
+
+        public ActionResult TransferSaldo()
+        {
+            ViewBag.Message = "Transfer Saldo";
+
+            return View();
+        }
+
+        public ActionResult TransferSaldo_2()
+        {
+            ViewBag.Message = "Tranfer Saldo 2";
+
+            return View();
+        }
+
+        public ActionResult RiwayatTransfer()
+        {
+            ViewBag.Message = "Riwayat Transfer";
+
+            return View();
+        }
+
+        public ActionResult BerhasilTransaksi()
+        {
+            ViewBag.Message = "Berhasil";
+
+            return View();
+        }
+
+        public ActionResult GagalTransaksi()
+        {
+            ViewBag.Message = "Gagal";
+
+            return View();
+        }
+
+        public ActionResult AkunBerhasil()
+        {
+            ViewBag.Message = "Berhasil";
+
+            return View();
+        }
+
+
+        public ActionResult AkunGagal()
+        {
+            ViewBag.Message = "Gagal";
+
+            return View();
+        }
+
+
 
 
 
